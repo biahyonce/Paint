dffeef28973405d94a48e78c7d1d496d38ed064f
BiancaCristina <biancacristina-silva@outlook.com>
2019-10-20 13:53:42 -0300

    Created function that draw circles

--- drawTool.py
+++ drawTool.py
@@ -5,12 +5,13 @@ from pygame import gfxdraw
 pygame.init()
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 screen = pygame.display.set_mode((400, 400), 0, 32)
 screen.fill(WHITE)
 
 pygame.display.flip()
 
-def drawLineX(x1, y1, x2, y2):
+def drawLineX(x1, y1, x2, y2, color):
     dx = x2 - x1
     dy = y2 - y1
     dxAbs = abs(dx)
@@ -24,7 +25,7 @@ def drawLineX(x1, y1, x2, y2):
     if (dx < 0 and dy < 0) or (dx > 0 and dy > 0): increment = 1
     else: increment = -1
 
-    screen.set_at((x,y), BLACK)
+    screen.set_at((x,y), color)
 
     while x < limit:
         x = x + 1
@@ -36,9 +37,9 @@ def drawLineX(x1, y1, x2, y2):
             y = y + increment
             diX = diX + 2 * (dyAbs - dxAbs)
         
-        screen.set_at((x,y), BLACK)
+        screen.set_at((x,y), color)
 
-def drawLineY(x1, y1, x2, y2):
+def drawLineY(x1, y1, x2, y2, color):
     dx = x2 - x1
     dy = y2 - y1
     dxAbs = abs(dx)
@@ -52,7 +53,7 @@ def drawLineY(x1, y1, x2, y2):
     if (dx < 0 and dy < 0) or (dx > 0 and dy > 0): increment = 1
     else: increment = -1
 
-    screen.set_at((x,y), BLACK) 
+    screen.set_at((x,y), color) 
 
     while y < limit:
         y = y + 1
@@ -64,9 +65,9 @@ def drawLineY(x1, y1, x2, y2):
             x = x + increment
             diY = diY + 2 * (dxAbs - dyAbs)
 
-        screen.set_at((x,y), BLACK) 
+        screen.set_at((x,y), color) 
    
-def drawLine(x1, y1, x2, y2):
+def drawLine(x1, y1, x2, y2, color):
     dx = x2 - x1
     dy = y2 - y1
     dxAbs = abs(dx)
@@ -75,10 +76,36 @@ def drawLine(x1, y1, x2, y2):
     diY = 2 * dxAbs - dyAbs
  
     if dyAbs <= dxAbs:
-        drawLineX(x1, y1, x2, y2)
+        drawLineX(x1, y1, x2, y2, color)
 
     else:
-        drawLineY(x1, y1, x2, y2)
+        drawLineY(x1, y1, x2, y2, color)
+
+def drawCircle(x2, y2, r, color):
+    x = 0
+    y = r
+    d = 1 - r
+
+    plotCircleOctect(x,y, x2, y2, color)
+
+    while y > x:
+        if d < 0: d += (2*x) + 3
+        else:
+            d += 2 * (x - y) + 5
+            y = y - 1
+
+        x = x + 1
+        plotCircleOctect(x,y, x2, y2, color)
+
+def plotCircleOctect(x1, y1, x2, y2, color):
+	screen.set_at((x1 + x2, y1 + y2), color)
+	screen.set_at((x2 - y1, x1 + y2), color)
+	screen.set_at((x2 - y1, y2 - x1), color)
+	screen.set_at((x2 - x1, y2 - y1), color)
+	screen.set_at((x2 - x1, y1 + y2), color)
+	screen.set_at((x1 + x2, y2 - y1), color)
+	screen.set_at((y1 + x2, y2 - x1), color)
+	screen.set_at((y1 + x2, x1 + y2), color)
 
 if __name__ == '__main__':
     sX = 0
@@ -100,10 +127,13 @@ if __name__ == '__main__':
             if event.type == pygame.MOUSEMOTION and line:
                 screen.fill(WHITE)
                 x, y = event.pos
-                drawLine(sX, sY, x, y)
+
+                # Calcula a distância entre pontos (define o raio)
+                r = int((((x - sX)**2) + ((y - sY)**2))**0.5)
+                drawCircle(sX, sY, r, BLACK)
                 pygame.display.flip()
 
             if event.type == pygame.MOUSEBUTTONUP and event.button == RIGHT:
                 line = False
-                
+        
         pygame.display.update()
